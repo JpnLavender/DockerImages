06dd1028e1645a3179caa69e0b13dd2c5ca48038
JpnLavender <nagisa20000014@gmail.com>
2016-10-03 10:52:55 +0900

    update siantra Dockerfile

---
+++ sinatra/models.rb
@@ -0,0 +1,20 @@
+require 'bundler/setup'
+Bundler.require
+require 'uri'
+#
+# configure :production, :development do
+#     db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/docker')
+#       pool = ENV["DB_POOL"] || ENV['MAX_THREADS'] || 5
+#         ActiveRecord::Base.establish_connection(
+#                 adapter:  db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+#                 host:      db.host,
+#                 username:  db.user,
+#                 password:  db.password,
+#                 database:  db.path[1..-1],
+#                 encoding:  'utf8',
+#                 pool:      pool
+#           )
+# end
+
+class User < ActiveRecord::Base
+end
