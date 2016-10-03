9ee7354a4889ff5682a44d1d2baba791ec066fc7
JpnLavender <nagisa20000014@gmail.com>
2016-10-03 16:47:09 +0900

    super commit

--- sinatra/models.rb
+++ sinatra/models.rb
@@ -1,21 +1,9 @@
 require 'bundler/setup'
 Bundler.require
-<<<<<<< HEAD
-require 'uri'
-#
-# configure :production, :development do
-#     db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/docker')
-#       pool = ENV["DB_POOL"] || ENV['MAX_THREADS'] || 5
-#         ActiveRecord::Base.establish_connection(
-#                 adapter:  db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-#                 host:      db.host,
-#                 username:  db.user,
-#                 password:  db.password,
-#                 database:  db.path[1..-1],
-#                 encoding:  'utf8',
-#                 pool:      pool
-#           )
-# end
+require 'active_record'
+
+ActiveRecord::Base.configurations = YAML.load_file('database.yml')
+# ActiveRecord::Base.establish_connection('production')
 
 class User < ActiveRecord::Base
 end
